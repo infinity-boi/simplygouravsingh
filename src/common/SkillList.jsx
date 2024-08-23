67258cb15884e5f86246cf499187ae07d2565357
infinity-boi <gouravsinghc2003@gmail.com>
2024-08-23 09:42:32 +0530

    Resolve Skill UI issue

--- src/common/SkillList.jsx
+++ src/common/SkillList.jsx
@@ -1,7 +1,7 @@
 function SkillList({ src, skill }) {
     return (
       <span>
-        <img src={src} alt="Checkmark icon" style={{width:"25px"}}/>
+        <img src={src} alt="Checkmark icon" style={{width:"25px", maxWidth:"30px", height:"25px"}}/>
         <p>{skill}</p>
       </span>
     );
